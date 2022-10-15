3d4a3d246cea5c3f9ee7dbc59b732538da48f983
Dwi Pamungkas <dw.pamungkas@gmail.com>
2022-10-15 16:48:04 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+//array string
+const cars = ['ayla', 'agya', 'calya', 'sigra', 'innova', 'kijang']
+//console.log('Janis Mobil', cars)
+console.log(cars[3])
+
+//length
+console.log('JUMLAH DATA', cars.length)
+
+//join
+console.log('JOIN', cars.join('-'))
+
+//pop
+console.log('POP', cars.pop())
+console.log(cars)
+
+
